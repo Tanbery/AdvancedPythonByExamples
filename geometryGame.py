5888c2378ecd77f90705a08b0573dde276b3307f
Tanbery <envergul23@yahoo.com>
2021-07-12 10:34:34 +0300

    add Geometry file

---
+++ geometryGame.py
@@ -0,0 +1,41 @@
+from random import randint
+
+#forward defination
+class GeoRect:
+	pass
+
+class GeoPoint:
+	def __init__(self,x,y):
+		self.x=x
+		self.y=y
+	def show(self):
+		return "(" + str(self.x) + "," + str(self.y) + ")"
+	def fall_in_rect(self,rect):
+		return ((rect.lwCorner.x <= self.x <= rect.upCorner.x) and (rect.lwCorner.y <= self.y <= rect.upCorner.y))
+	
+
+class GeoRect:
+	def __init__(self,p1,p2):
+		self.lwCorner  = p1
+		self.upCorner  = p2
+		
+
+	def show(self):
+		return "{" + self.lwCorner.show() + "} {" + self.upCorner.show() + "}"
+
+def is_point_in_rect(p,rect):
+	return ((rect.lwCorner.x <= p.x <= rect.upCorner.x) and (rect.lwCorner.y <= p.y <= rect.upCorner.y))
+	
+p1 = GeoPoint(randint( 0, 50)	, randint( 0, 50))
+p2 = GeoPoint(randint(50,100)	, randint(50,100))
+
+print("Point1: " + p1.show())
+print("Point2: " + p2.show())
+
+p0 = GeoPoint(int(input("Guess X:")),int(input("Guess Y:")))
+print("Point0: " + p0.show())
+
+geo_rect= GeoRect(p1,p2)
+print(geo_rect.show())
+
+print(p0.fall_in_rect(geo_rect))
